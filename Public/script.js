319c16038cc41d117f046e86f645be684c31f706
Christian Altenkamp <altenkamp1@gmail.com>
2023-05-26 11:39:18 +1000

    Replacing task instances with book

--- Public/script.js
+++ Public/script.js
@@ -1,64 +1,64 @@
 // Setting up variables for our HTML elements using DOM selection
-const form = document.getElementById("taskform");
-const tasklist = document.getElementById("tasklist");
+const form = document.getElementById("bookform");
+const booklist = document.getElementById("booklist");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  console.log(form.elements.taskType.value)
+  console.log(form.elements.bookType.value)
 
-  addTask(
-    form.elements.taskName.value,
-    form.elements.taskType.value,
-    form.elements.taskRate.value,
-    form.elements.taskTime.value,
-    form.elements.taskClient.value,
+  addBook(
+    form.elements.bookName.value,
+    form.elements.bookType.value,
+    form.elements.bookRate.value,
+    form.elements.bookTime.value,
+    form.elements.bookClient.value,
   )
-  // console.log(taskList)
+  // console.log(bookList)
 })
 
-function displayTasks() {
+function displayBooks() {
 
-  tasklist.innerHTML = "";
+  booklist.innerHTML = "";
 
-  let localTasks = JSON.parse(localStorage.getItem('tasks'));
+  let localBooks = JSON.parse(localStorage.getItem('books'));
 
-  if (localTasks !== null) {
+  if (localBooks !== null) {
 
-    localTasks.forEach((task) => {
+    localBooks.forEach((book) => {
 
-      console.log(task)
+      console.log(book)
 
-      // Create task items for the DOM and add to the list
+      // Create book items for the DOM and add to the list
       let item = document.createElement("li");
-      item.setAttribute("data-id", task.id);
-      item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
-      tasklist.appendChild(item);
+      item.setAttribute("data-id", book.id);
+      item.innerHTML = `<p><strong>${book.name}</strong><br>${book.type}</p>`;
+      booklist.appendChild(item);
 
-      // Clear the value of the input once the task has been added to the page
+      // Clear the value of the input once the book has been added to the page
       form.reset();
 
       // Setup delete button DOM elements
       let delButton = document.createElement("button");
       let delButtonText = document.createTextNode("Delete");
       delButton.appendChild(delButtonText);
-      item.appendChild(delButton); // Adds a delete button to every task
+      item.appendChild(delButton); // Adds a delete button to every book
 
       // Listen for when the delete button is clicked
       delButton.addEventListener("click", function(event) {
 
-        localTasks.forEach(function(taskArrayElement, taskArrayIndex) {
-          if (taskArrayElement.id == item.getAttribute('data-id')) {
-            localTasks.splice(taskArrayIndex, 1)
+        localBooks.forEach(function(bookArrayElement, bookArrayIndex) {
+          if (bookArrayElement.id == item.getAttribute('data-id')) {
+            localBooks.splice(bookArrayIndex, 1)
           }
         })
 
-        localStorage.setItem('tasks', JSON.stringify(localTasks));
+        localStorage.setItem('books', JSON.stringify(localBooks));
 
         // Make sure the deletion worked by logging out the whole array
-        // console.log(taskList)
+        // console.log(bookList)
 
-        item.remove(); // Remove the task item from the page when button clicked
+        item.remove(); // Remove the book item from the page when button clicked
         // Because we used 'let' to define the item, this will always delete the right element
 
       })
@@ -71,12 +71,12 @@ function displayTasks() {
 
 
 
-// Create an object called 'task'
+// Create an object called 'book'
 // Populate the properties based on the provided data model
 
 // Commented out now the object creation is included in the function
 
-// var task = {
+// var book = {
 //   name: "Initial Sketches",
 //   type: "Concept Ideation",
 //   id: Date.now(),
@@ -86,20 +86,20 @@ function displayTasks() {
 //   client: "Google"
 // }
 
-// console.log(task);
+// console.log(books);
 
 
-// Create a function called 'addTask'
+// Create a function called 'addBook'
 // Give the function input parameters for: name, type, rate, time, client
 // Paste your object definition from above in the function
 // Replace the property values with the input paramaters
-// Add the object to the taskList array
+// Add the object to the bookList array
 
-  function addTask(name, type, rate, time, client) {
+  function addBook(name, type, rate, time, client) {
 
     // Creating the object with the usual property:value syntax
-    // Create task object 
-    // let task = {
+    // Create book object 
+    // let book = {
     //   name: name,
     //   type: type,
     //   id: Date.now(),
@@ -110,7 +110,7 @@ function displayTasks() {
     // }
 
     // Creating the object, directly passing in the input parameters
-    let task = {
+    let book = {
       name,
       type,
       id: Date.now(),
@@ -121,27 +121,27 @@ function displayTasks() {
     }
 
     // fetching and parse localStorage value
-    let localTasks = JSON.parse(localStorage.getItem('tasks'));
+    let localBooks = JSON.parse(localStorage.getItem('books'));
 
-    if (localTasks == null){
-      localTasks = [task];
+    if (localBooks == null){
+      localBooks = [book];
     } else {
-      // Check to see if there is an existing task
-      if (localTasks.find(element => element.id === task.id)) {
-        console.log('Task ID already exists');
+      // Check to see if there is an existing book
+      if (localBooks.find(element => element.id === book.id)) {
+        console.log('Books ID already exists');
       } else {
-        localTasks.push(task);
+        localBooks.push(book);
       }
     }
 
-    localStorage.setItem('tasks', JSON.stringify(localTasks))
+    localStorage.setItem('books', JSON.stringify(localBooks))
 
-    displayTasks();
+    displayBooks();
 
   }
 
 // Call the function with test values for the input paramaters
-addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
-displayTasks();
+addBook("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+displayBooks();
 // Log the array to the console.
-console.log(localTasks);
+console.log(localBooks);
